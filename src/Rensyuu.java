a3d2bea8af65192aeca84ebf170141e4472d4b02
Luciva33 <luciva0303@gmail.com>
2024-01-09 16:38:04 +0900

    初めてのコミット

---
+++ src/Rensyuu.java
@@ -0,0 +1,12 @@
+
+public class Rensyuu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int tate = 3;
+		int yoko = 5;
+		int kotae = tate * yoko;
+		System.out.println("縦幅3横幅５の長方形の面積は、" + kotae);
+	}
+
+}
